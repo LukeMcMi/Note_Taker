7cafa56a0cb13423c53621c514b88da73a0e17d6
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-11-07 17:00:59 +1100

    Delete function added to notes.js. tested and working.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,6 @@
 const nt = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { writeToFile, readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
 nt.get('/', (req, res) =>
@@ -33,6 +33,36 @@ nt.post('/', (req, res) => {
   } else {
     res.json('Error in posting note');
   }
+});
+
+// Get route for a specific note
+nt.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+  .then((data) => JSON.parse(data))
+  .then((json) => {
+    const result = json.filter((note) => note.id === noteId);
+    return result.length > 0
+    ? res.json(result)
+    : res.json("No note with that ID");
+  });
+});
+
+// Delete route for a specific note
+nt.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
 });
 
 module.exports = nt;
